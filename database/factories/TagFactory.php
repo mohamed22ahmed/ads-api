fc7871d4d095dc5faaf4bb1103ae927ec3c573ef
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2022-12-20 23:26:26 +0200

    adding api controllers

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -15,7 +15,7 @@ class TagFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => Str::random(10),
